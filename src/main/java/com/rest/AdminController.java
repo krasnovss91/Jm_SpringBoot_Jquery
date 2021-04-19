e3d7738c3410256e7d9a6bde1bfb0063306a2023
krasnovss91 <krasnovstanislav91@gmail.com>
2021-04-19 14:24:12 +0400

    ResponceEntity_6

--- src/main/java/com/rest/AdminController.java
+++ src/main/java/com/rest/AdminController.java
@@ -4,9 +4,9 @@ import com.model.User;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
 
 import java.security.Principal;
 import java.util.List;
@@ -19,43 +19,36 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
-//вместо юзера возвращать запрос целиком
- //ResponceEntity.ok().head().body()
     @GetMapping("{id}")
     public ResponseEntity<User> getById(@PathVariable Long id) {
-
-        //return this.userService.getUserById(id);
-        return new ResponseEntity<User>(this.userService.getUserById(id),HttpStatus.OK);
+        return new ResponseEntity<User>(this.userService.getUserById(id), HttpStatus.OK);
     }
 
     @GetMapping("/findAllUsers")
-    public List<User> findAllUsers() {
-        List<User> list = userService.getAllUsers();
-        return list;
+    public ResponseEntity<List<User>> findAllUsers() {
+        return new ResponseEntity<List<User>>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @PostMapping("/add")
     public ResponseEntity<User> newUser(@RequestBody User user) {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        //return userService.saveUser(user);
-        return new ResponseEntity<User>(userService.saveUser(user),HttpStatus.OK);
+        return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.OK);
     }
 
     @PutMapping("/edit")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
-        return new ResponseEntity<User>(userService.saveUser(user),HttpStatus.OK);
-        //return userService.saveUser(user);
+        return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteUsers(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteUsers(@PathVariable Long id) {
         userService.deleteUser(id);
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     @GetMapping("/findUser")
     public ResponseEntity<User> userData(Principal principal) {
-        //return userService.findUserByName(principal.getName());
-        return new ResponseEntity<User>(userService.findUserByName(principal.getName()),HttpStatus.OK);
+        return new ResponseEntity<User>(userService.findUserByName(principal.getName()), HttpStatus.OK);
     }
 
 }
