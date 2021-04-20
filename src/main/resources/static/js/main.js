aff250b8852cad4edc1a8449bb263a17add5d1db
krasnovss91 <krasnovstanislav91@gmail.com>
2021-04-20 17:34:38 +0400

    Roles_106

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -142,11 +142,16 @@ function createTableRow(u) {
 //таблица на вкладке user
 function createAuTableRow(u) {
     let roleUser = null
+    roleUser = u.roles.map(function (role){
+        return  role.name
+    });
+    /*
     for (var i = 0; i < u.roles.length; i++) {
         if (u.roles[i].name === "ROLE_ADMIN") roleUser = "ADMIN"
         else if (u.roles[i].name === "ROLE_USER") roleUser = "USER"
 
     }
+     */
 
     return `<tr id="user_table_row_${u.id}">
                 <td>${u.id}</td>
